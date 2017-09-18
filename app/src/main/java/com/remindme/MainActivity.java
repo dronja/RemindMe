093602db0bddb4b01459a7546049f52b9ff57a6a
Vladimir <vladimir@crypviser.net>
2017-09-18 15:08:22 +0300

    Add NavigationView item

--- app/src/main/java/com/remindme/MainActivity.java
+++ app/src/main/java/com/remindme/MainActivity.java
@@ -1,19 +1,25 @@
 package com.remindme;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private static final int LAYOUT = R.layout.activity_main;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
         
         initToolbar();
+        initNavigationView();
     }
 
     private void initToolbar() {
@@ -28,5 +34,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
     }
 }
