ee6ada0d15e7b5ad9e0572e9b2b1865a5477dcaa
Vladimir <vladimir@crypviser.net>
2017-09-18 18:12:34 +0300

    Add tab navigation

--- app/src/main/java/com/remindme/MainActivity.java
+++ app/src/main/java/com/remindme/MainActivity.java
@@ -1,15 +1,25 @@
 package com.remindme;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import com.remindme.adapter.TabPagerFragmentAdapter;
+import com.remindme.constant.Constans;
+
 public class MainActivity extends AppCompatActivity {
+    private static final int LAYOUT = R.layout.activity_main;
+
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
-    private static final int LAYOUT = R.layout.activity_main;
+    private ViewPager viewPager;
 
 
     @Override
@@ -17,9 +27,12 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(LAYOUT);
-        
+
         initToolbar();
         initNavigationView();
+        initTabs();
+
+
     }
 
     private void initToolbar() {
@@ -38,5 +51,37 @@ public class MainActivity extends AppCompatActivity {
 
     private void initNavigationView() {
         drawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView)findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawers();
+                switch (item.getItemId()){
+                    case R.id.actionNotificationItem:
+                        showNotificationTab();
+                }
+                return true;
+            }
+        });
+    }
+
+    private void initTabs() {
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+
+        TabPagerFragmentAdapter adapter = new TabPagerFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout  = (TabLayout)findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    private void showNotificationTab(){
+        viewPager.setCurrentItem(Constans.TAB_TWO);
     }
 }
---
+++ app/src/main/java/com/remindme/constant/Constans.java
@@ -0,0 +1,9 @@
+package com.remindme.constant;
+
+
+
+public class Constans {
+    public static final int TAB_ONE = 0;
+    public static final int TAB_TWO = 1;
+    public static final int TAB_TREE = 2;
+}
